702948fed0aa447556cae0c2d85326b54b0c5d34
wilfredodtorres-glitch <wilfredo.d.torres@gmail.com>
2026-08-04 16:48:26 -0600

    Summary

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
