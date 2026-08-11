8b1aaafd8d04d65eff748bcc48fc971fb6a8c955
wilfredodtorres-glitch <wilfredo.d.torres@gmail.com>
2026-08-11 16:33:51 -0600

    Update layout.js
    etste

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 export default function MainLayout({ children }) {
   return (
     <>
+      <Toaster position="top-right" />
       <Header />
       {children}
       <Footer />
